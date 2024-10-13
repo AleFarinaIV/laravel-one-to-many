f625b551cfde4fc4647bf131d97a2f536b10f7d5
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-10-13 14:17:25 +0200

    [UPD] updated create.blade file layout, added select for project types

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -6,67 +6,98 @@
             <div class="col-12">
                 <form action="{{ route('admin.projects.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
-                    <div class="form-group py-2">
-                        <label for="name">Name</label>
-                        <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
-                            name="name" value="{{ old('name') }}" required>
-                        @error('name')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="form-group py-2">
-                        <label for="description">Description</label>
-                        <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description"
-                            rows="3" required>{{ old('description') }}</textarea>
-                        @error('description')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="form-group py-2">
-                        <label for="start_date">Start Date</label>
-                        <input type="date" class="form-control @error('start_date') is-invalid @enderror" id="start_date"
-                            name="start_date" value="{{ old('start_date') }}" required>
-                        @error('start_date')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="form-group py-2">
-                        <label for="end_date">End Date</label>
-                        <input type="date" class="form-control @error('end_date') is-invalid @enderror" id="end_date"
-                            name="end_date" value="{{ old('end_date') }}" required>
-                        @error('end_date')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="form-group py-2">
-                        <label for="stack">Stack</label>
-                        <input type="text" class="form-control @error('stack') is-invalid @enderror" id="stack"
-                            name="stack" value="{{ old('stack') }}" required>
-                        @error('stack')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
                     <div class="row">
-                        <div class="col-4">
-                            <div class="form-group py-2 mb-4">
-                                <label for="status">Status</label>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="status" name="status" value="1"
-                                        {{ old('status') ? 'checked' : '' }}>
-                                    <label class="form-check-label" for="status">
-                                        Compleated
-                                    </label>
-                                </div>
-                                @error('status')
+                        <div class="col-6">
+                            <div class="form-group py-2">
+                                <label for="name">Name</label>
+                                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                                    name="name" value="{{ old('name') }}" required>
+                                @error('name')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
                         </div>
-                        <div class="col-8">
+                        <div class="col-6">
                             <div class="form-group py-2">
+                                <label for="stack">Stack</label>
+                                <input type="text" class="form-control @error('stack') is-invalid @enderror" id="stack"
+                                    name="stack" value="{{ old('stack') }}" required>
+                                @error('stack')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group py-2">
+                                <label for="description">Description</label>
+                                <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description"
+                                    rows="3" required>{{ old('description') }}</textarea>
+                                @error('description')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="row">
+                                <div class="col-4">
+                                    <div class="form-group py-2">
+                                        <label for="start_date">Start Date</label>
+                                        <input type="date" class="form-control @error('start_date') is-invalid @enderror" id="start_date"
+                                            name="start_date" value="{{ old('start_date') }}" required>
+                                        @error('start_date')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="form-group py-2">
+                                        <label for="end_date">End Date</label>
+                                        <input type="date" class="form-control @error('end_date') is-invalid @enderror" id="end_date"
+                                            name="end_date" value="{{ old('end_date') }}" required>
+                                        @error('end_date')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="form-group py-2">
+                                        <label for="status">Status</label>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" id="status" name="status" value="1"
+                                                {{ old('status') ? 'checked' : '' }}>
+                                            <label class="form-check-label" for="status">
+                                                Compleated
+                                            </label>
+                                        </div>
+                                        @error('status')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-4">
+                        <div class="col-6">
+                            <div class="form-group">
                                 <label for="image_path">Image</label>
                                 <input type="file" class="form-control @error('end_date') is-invalid @enderror" 
                                     name="image_path" id="image_path">
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="form-group">
+                                <label for="type_id">Project Type</label>
+                                <select name="type_id" id="type_id" class="form-select" required>
+                                    <option value="">-- Select a type --</option>
+                                    @foreach($types as $type)
+                                        <option value="{{ $type->id }}" @selected($type->id == old('type_id'))>
+                                            {{ $type->name }}
+                                        </option>
+                                    @endforeach
+                                </select>
                             </div>
                         </div>
                     </div>
